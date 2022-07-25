7d101d98af2dde5584cc0417a9ae9a0f42d96ffe
Om <omkaushik015@gmail.com>
2022-07-25 19:39:12 +0530

    add gui to the game

--- script.js
+++ script.js
@@ -1,84 +1,108 @@
-// Function to randomly  generate 'Rock', 'Paper' or 'Scissors'.
-function computerPlay(){
-    let randomNumber = Math.floor((Math.random() * 100) + 1);
-if(randomNumber <= 33){
-  return "rock";
-}
-else if(randomNumber >= 66){
-  return "paper";
-}
-else{
-  return "scissors";
-}
-}
+let playerSelection;
+let computerSelection;
+let playerScore = document.querySelector(".score #playerScore");
+let computerScore = document.querySelector(".score #computerScore");
+playerScore.innerText = 0;
+computerScore.innerText = 0;
 
-let playerscore = 0 , computerscore = 0;
+let rockButton = document.querySelector(".images#rock");
+let paperButton = document.querySelector(".images#paper");
+let scissorsButton = document.querySelector(".images#scissors");
+
+let playerDisplay = document.querySelector(".player .image");
+let computerDisplay = document.querySelector(".computer .image");
+
+let text = document.querySelector(".text");
+
+let playerBorder = document.querySelector(".player .image");
+let computerBorder = document.querySelector(".computer .image");
+
+rockButton.addEventListener("click", (e) => {
+  playerSelection = "rock";
+  playerDisplay.innerHTML = ' <img src="./images/rock.png" alt="rock" />';
+  game();
+});
+paperButton.addEventListener("click", (e) => {
+  playerSelection = "paper";
+  playerDisplay.innerHTML = ' <img src="./images/paper.png" alt="rock" />';
+  game();
+});
+scissorsButton.addEventListener("click", (e) => {
+  playerSelection = "scissors";
+  playerDisplay.innerHTML = ' <img src="./images/scissors.png" alt="rock" />';
+  game();
+});
+
+// function to play five rounds with computer.
+
+function game() {
+  computerSelection = computerPlay();
+  computerDisplay.innerHTML = `<img src="./images/${computerSelection}.png" alt="${computerSelection}" />`;
+  console.log(playerSelection);
+  console.log(computerSelection);
+  text.innerText = play(playerSelection, computerSelection);
+
+  if (playerScore.innerText == "5") {
+    text.innerText = "You won the series!🎉";
+    playerScore.innerText = 0;
+    computerScore.innerText = 0;
+  } else if (computerScore.innerText == "5") {
+    text.innerText = "You lost the series.😔";
+    playerScore.innerText = 0;
+    computerScore.innerText = 0;
+  }
+}
 
 // Function to compare player's selection with computer's selection
 
-function play(playerSelection, computerSelection){
-
-
-if(playerSelection === computerSelection){
-  return `It's a draw`;
-}
-
-else if(playerSelection === "rock" && computerSelection === "paper"){
-    computerscore++;
+function play(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    playerBorder.style.boxShadow = "0 5px 10px rgba(0, 0, 0, 0.5)";
+    computerBorder.style.boxShadow = "0 5px 10px rgba(0, 0, 0, 0.5)";
+    return `It's a draw`;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore.innerText++;
+    playerBorder.style.boxShadow = "0 5px 10px rgba(250, 0, 0, 0.7)";
+    computerBorder.style.boxShadow = "0 5px 10px rgba(0, 128, 255, 0.7)";
     return `You lost! Paper beats Rock.`;
-}
-else if(playerSelection === "rock" && computerSelection === "scissors"){
-    playerscore++;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore.innerText++;
+    computerBorder.style.boxShadow = "0 5px 10px rgba(250, 0, 0, 0.7)";
+    playerBorder.style.boxShadow = "0 5px 10px rgba(0, 128, 255, 0.7)";
     return `You won! Rock beats Scissors.`;
-}
-
-else if(playerSelection === "paper" && computerSelection === "rock"){
-    playerscore++;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore.innerText++;
+    computerBorder.style.boxShadow = "0 5px 10px rgba(250, 0, 0, 0.7)";
+    playerBorder.style.boxShadow = "0 5px 10px rgba(0, 128, 255, 0.7)";
     return `You won! Paper beats Rock.`;
-}
-else if(playerSelection === "paper" && computerSelection === "scissors"){
-    computerscore++;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore.innerText++;
+    playerBorder.style.boxShadow = "0 5px 10px rgba(250, 0, 0, 0.7)";
+    computerBorder.style.boxShadow = "0 5px 10px rgba(0, 128, 255, 0.7)";
     return `You lost! Scissors beats Paper`;
-}
-
-else if(playerSelection === "scissors" && computerSelection === "rock"){
-    computerscore++;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore.innerText++;
+    playerBorder.style.boxShadow = "0 5px 10px rgba(250, 0, 0, 0.7)";
+    computerBorder.style.boxShadow = "0 5px 10px rgba(0, 128, 255, 0.7)";
     return `You lost! Rock beats Scissors`;
-}
-else if(playerSelection === "scissors" && computerSelection === "paper"){
-    playerscore++;
-  return `You won! Scissors beats Paper`;
-}
-else{
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore.innerText++;
+    computerBorder.style.boxShadow = "0 5px 10px rgba(250, 0, 0, 0.7)";
+    playerBorder.style.boxShadow = "0 5px 10px rgba(0, 128, 255, 0.7)";
+    return `You won! Scissors beats Paper`;
+  } else {
     console.log("Error");
+  }
 }
 
+// Function to randomly  generate 'Rock', 'Paper' or 'Scissors'.
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 100 + 1);
+  if (randomNumber <= 33) {
+    return "rock";
+  } else if (randomNumber >= 66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
-
-// Function to play five rounds with computer.
-function game(){
-   
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose Rock, Paper or Scissors","");
-        let computerSelection = computerPlay();
-        playerSelection = playerSelection.toLowerCase();
-        computerSelection = computerSelection.toLowerCase();
-        console.log(playerSelection);
-        console.log(computerSelection);
-
-       alert(play(playerSelection,computerSelection));
-    
-    }
-    // The following block of code will display the final scores.
-    if(playerscore === computerscore){
-        alert(`Your score = ${playerscore}  Computer Score = ${computerscore}\nIt's a draw!`);
-    }
-    else if(playerscore > computerscore){
-        alert(`Your score = ${playerscore}  Computer Score = ${computerscore}\n You won!`);
-    }
-    else{
-        alert(`Your score = ${playerscore}  Computer Score = ${computerscore}\nYou lost!`);
-    }
-    
-}
-game();
